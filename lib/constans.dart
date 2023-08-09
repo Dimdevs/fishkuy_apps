b8ce30980dcf18c313847498eab4bcad4fed0635
Dimas Maulana <dimasmaulana9494@gmail.com>
2023-08-10 01:34:26 +0700

    push form login

---
+++ lib/constans.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color WhiteColor = Color(0xffffffff);
+Color textColor = Color(0xff0D0140);
+Color secondaryTextColor = Color(0xff524B6B);
+Color buttonColor = Color(0xffE6E1FF);
+Color primaryButtonColor = Color(0xff130160);
+Color tncButtonColor = Color(0xff130160);
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: WhiteColor,
+);
+
+TextStyle textTextStyle = GoogleFonts.dmSans(
+  color: textColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(
+  color: secondaryTextColor,
+);
+
+TextStyle tncTextStyle = GoogleFonts.dmSans(
+  color: tncButtonColor,
+);
+
+FontWeight bold = FontWeight.bold;
